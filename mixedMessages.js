c1820a3d27eff8562459ff8effaa6926caf451c3
Darick <darickmcbride@me.com>
2023-04-06 13:22:30 -0700

    added a message class

--- mixedMessages.js
+++ mixedMessages.js
@@ -1,1 +1,18 @@
-console.log('Hello World')
+'use strict';
+
+let potterData = require('./data/harryPotter.json');
+
+class message {
+    constructor(wands, houses) {
+        const wandsData = potterData.map(item => item.Wand)
+        const filteredWands = wandsData.filter(wand => wand !== 'Unknown' && wand !== '' && !wand.includes('Inherited'))
+        wands = filteredWands
+        houses = ['Gryffindor', 'Hufflepuff', 'Ravenclaw', 'Slytherin']
+        this.wands = wands
+        this.houses = houses
+    }
+}
+
+
+const message1 = new message();
+console.log(message1.wands)
